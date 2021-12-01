c2418dfa8a722c43e67937152ba9a5e2fa10ef59
nsunderland1 <sunderland.nicholas@protonmail.com>
2021-11-30 20:20:08 -0800

    Forgot to uncomment a couple of lines

--- start_day.bash
+++ start_day.bash
@@ -3,7 +3,7 @@
 DAY=$(env TZ=America/Toronto date +%-d)
 DAY_PADDED=$(printf "%02d" $DAY)
 
-# firefox https://adventofcode.com/2020/day/${DAY}
+firefox https://adventofcode.com/2020/day/${DAY}
 
 cargo new day${DAY_PADDED}
 cargo run -p update_workspace -- ./Cargo.toml day${DAY_PADDED}
@@ -11,6 +11,6 @@ cp template.rs day${DAY_PADDED}/src/main.rs
 
 code -n . day${DAY_PADDED}/src/main.rs
 
-# curl "https://adventofcode.com/2021/day/${DAY}/input" -H "Cookie: session=${ADVENT_SESSION}" > "day${DAY_PADDED}/input"
+curl "https://adventofcode.com/2021/day/${DAY}/input" -H "Cookie: session=${ADVENT_SESSION}" > "day${DAY_PADDED}/input"
 
 cd day${DAY_PADDED} && cargo build && cargo build --release
