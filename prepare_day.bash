f9a5c27350c985916c59c5ca612224039b00e025
nsunderland1 <sunderland.nicholas@protonmail.com>
2021-12-05 13:45:27 -0800

    Add prep script

---
+++ prepare_day.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DAY=$(($(env TZ=America/Toronto date +%-d)+1))
+DAY_PADDED=$(printf "%02d" $DAY)
+
+cp template.rs src/day${DAY_PADDED}.rs
+
+echo "Don't forget to update src/lib.rs with the new day!"
+
+code src/lib.rs
