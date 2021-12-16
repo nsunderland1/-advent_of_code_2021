ec9518455bc5bf07fb33309fea359357a7012d92
nsunderland1 <sunderland.nicholas@protonmail.com>
2021-12-15 19:59:47 -0800

    Start of a likely doomed attempt to optimize day 5

--- src/day05.rs
+++ src/day05.rs
@@ -1,9 +1,88 @@
-use itertools::{Either, Itertools};
+use std::{
+    cmp::{max, min},
+    collections::HashSet,
+};
 
-use crate::grid::Grid;
+use itertools::Itertools;
 
-fn parse_line(s: &str) -> ((usize, usize), (usize, usize)) {
-    s.split(" -> ")
+struct Vertical {
+    x: usize,
+    min_y: usize,
+    max_y: usize,
+}
+
+struct Horizontal {
+    y: usize,
+    min_x: usize,
+    max_x: usize,
+}
+
+enum Line {
+    Horizontal(Horizontal),
+    Vertical(Vertical),
+    Diagonal {
+        start: (usize, usize),
+        end: (usize, usize),
+    }, // TODO: split
+}
+
+impl Line {
+    fn new(start: (usize, usize), end: (usize, usize)) -> Self {
+        if start.0 == end.0 {
+            Self::Vertical(Vertical {
+                x: start.0,
+                min_y: min(start.1, end.1),
+                max_y: max(start.1, end.1),
+            })
+        } else if start.1 == end.1 {
+            Self::Horizontal(Horizontal {
+                y: start.1,
+                min_x: min(start.0, end.0),
+                max_x: max(start.0, end.0),
+            })
+        } else {
+            Self::Diagonal { start, end }
+        }
+    }
+
+    fn intersections(&self, other: &Line) -> Vec<(usize, usize)> {
+        match (self, other) {
+            (Self::Vertical(vertical), Self::Horizontal(horizontal))
+            | (Self::Horizontal(horizontal), Self::Vertical(vertical)) => {
+                if (horizontal.min_x..=horizontal.max_x).contains(&vertical.x)
+                    && (vertical.min_y..=vertical.max_y).contains(&horizontal.y)
+                {
+                    vec![(vertical.x, horizontal.y)]
+                } else {
+                    vec![]
+                }
+            }
+            (Self::Vertical(v1), Self::Vertical(v2)) => {
+                if v1.x == v2.x && v1.min_y <= v2.max_y && v2.min_y <= v1.max_y {
+                    (v1.min_y.clamp(v2.min_y, v2.max_y)..v1.max_y.clamp(v2.min_y, v2.max_y))
+                        .map(|y| (v1.x, y))
+                        .collect()
+                } else {
+                    vec![]
+                }
+            }
+            (Self::Horizontal(h1), Self::Horizontal(h2)) => {
+                if h1.y == h2.y && h1.min_x <= h2.max_x && h2.min_x <= h1.max_x {
+                    (h1.min_x.clamp(h2.min_x, h2.max_x)..h1.max_x.clamp(h2.min_x, h2.max_x))
+                        .map(|x| (x, h1.y))
+                        .collect()
+                } else {
+                    vec![]
+                }
+            }
+            (Self::Diagonal { .. }, _) | (_, Self::Diagonal { .. }) => unreachable!(), // skip for now
+        }
+    }
+}
+
+fn parse_line(s: &str) -> Line {
+    let (start, end) = s
+        .split(" -> ")
         .map(|s| {
             s.split(',')
                 .map(str::parse)
@@ -12,50 +91,27 @@ fn parse_line(s: &str) -> ((usize, usize), (usize, usize)) {
                 .unwrap()
         })
         .collect_tuple()
-        .unwrap()
+        .unwrap();
+    Line::new(start, end)
 }
 
-pub fn count_overlaps(
-    input: &Vec<((usize, usize), (usize, usize))>,
-    include_diagonals: bool,
-) -> usize {
-    let mut grid: Grid<usize> = Grid::new(1000, 1000);
-    for (start, end) in input.iter() {
-        if start.0 == end.0 {
-            let min = std::cmp::min(start.1, end.1);
-            let max = std::cmp::max(start.1, end.1);
-            for y in min..=max {
-                grid[(start.0, y)] += 1;
-            }
-        } else if start.1 == end.1 {
-            let min = std::cmp::min(start.0, end.0);
-            let max = std::cmp::max(start.0, end.0);
-            for x in min..=max {
-                grid[(x, start.1)] += 1;
-            }
-        } else if include_diagonals {
-            let x_range = if start.0 < end.0 {
-                Either::Left(start.0..=end.0)
-            } else {
-                Either::Right((end.0..=start.0).rev())
-            };
-            let y_range = if start.1 < end.1 {
-                Either::Left(start.1..=end.1)
-            } else {
-                Either::Right((end.1..=start.1).rev())
-            };
-            for (x, y) in x_range.zip(y_range) {
-                grid[(x, y)] += 1;
-            }
-        }
-    }
-
-    grid.into_flat_iter().filter(|&n| n > 1).count()
+fn count_overlaps(lines: &Vec<Line>) -> usize {
+    lines
+        .iter()
+        .tuple_combinations()
+        .flat_map(|(l, r)| l.intersections(r).into_iter())
+        .sorted()
+        .dedup()
+        .count()
 }
 
 pub fn run(input: &str) {
-    let input: Vec<_> = input.lines().map(parse_line).collect();
+    let lines: Vec<_> = input.lines().map(parse_line).collect();
+    let lines = lines
+        .into_iter()
+        .filter(|line| !matches!(line, Line::Diagonal { .. }))
+        .collect_vec();
 
-    println!("Part 1: {}", count_overlaps(&input, false));
-    println!("Part 2: {}", count_overlaps(&input, true));
+    println!("Part 1: {}", count_overlaps(&lines));
+    // println!("Part 2: {}", count_overlaps(&input, true));
 }
