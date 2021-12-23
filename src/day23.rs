ea8cf0edc2f04a8202906e4276af6d3ad2ab3fde
nsunderland1 <sunderland.nicholas@protonmail.com>
2021-12-22 21:52:53 -0800

    OMG LEADERBOARDED

--- src/day23.rs
+++ src/day23.rs
@@ -1,12 +1,10 @@
+// note: doesn't work for the example input
 pub fn run(input: &str) {
     let result1 = 14460; // computed by hand
 
     println!("Part 1: {}", result1);
 
-    let result2 = {
-        // Part 2
-        0
-    };
+    let result2 = 40866; // also computed by hand
 
     println!("Part 2: {}", result2);
 }
