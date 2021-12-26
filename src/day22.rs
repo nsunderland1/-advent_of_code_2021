7fca31abe56eb2fae37569a1f10fe2fc6c4cdcb7
nsunderland1 <sunderland.nicholas@protonmail.com>
2021-12-25 18:25:58 -0800

    Combine both parts of day 22

--- src/day22.rs
+++ src/day22.rs
@@ -1,6 +1,6 @@
 use std::ops::RangeInclusive;
 
-use itertools::{iproduct, Itertools};
+use itertools::Itertools;
 use nom::{
     branch::alt,
     bytes::complete::tag,
@@ -51,15 +51,6 @@ fn parse_step(input: &str) -> IResult<&str, Step> {
     )(input)
 }
 
-fn index((x, y, z): (isize, isize, isize)) -> Option<(usize, usize, usize)> {
-    let (x, y, z) = (x + 50, y + 50, z + 50);
-    if x >= 0 && y >= 0 && z >= 0 && x <= 100 && y <= 100 && z <= 100 {
-        Some((x as usize, y as usize, z as usize))
-    } else {
-        None
-    }
-}
-
 // Important that Start < End, otherwise edge cases won't be handled right
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 enum EventKind {
@@ -80,193 +71,196 @@ pub fn run(input: &str) {
         .map(|line| parse_step(&line).unwrap().1)
         .collect();
 
-    let mut grid = vec![vec![vec![false; 101]; 101]; 101];
+    let x_events = steps
+        .iter()
+        .enumerate()
+        .flat_map(|(i, step)| {
+            [
+                Event {
+                    kind: EventKind::Start,
+                    position: *step.x.start(),
+                    step_id: i,
+                },
+                Event {
+                    kind: EventKind::End,
+                    position: *step.x.end(),
+                    step_id: i,
+                },
+            ]
+        })
+        .sorted_by_key(|event| (event.position, event.kind));
 
-    let result1 = {
-        for step in steps.iter() {
-            if *step.x.start() > 50
-                || *step.x.end() < -50
-                || *step.y.start() > 50
-                || *step.y.end() < -50
-                || *step.z.start() > 50
-                || *step.z.end() < -50
-            {
-                continue;
-            }
-            for (x, y, z) in
-                iproduct!(step.x.clone(), step.y.clone(), step.z.clone()).filter_map(index)
-            {
-                grid[x][y][z] = step.state;
-            }
-        }
+    let y_events = steps
+        .iter()
+        .enumerate()
+        .map(|(i, step)| {
+            [
+                Event {
+                    kind: EventKind::Start,
+                    position: *step.y.start(),
+                    step_id: i,
+                },
+                Event {
+                    kind: EventKind::End,
+                    position: *step.y.end(),
+                    step_id: i,
+                },
+            ]
+        })
+        .collect_vec();
 
-        grid.iter()
-            .flat_map(|plane| plane.iter().map(|row| row.iter().filter(|v| **v).count()))
-            .sum::<usize>()
-    };
+    let z_events = steps
+        .iter()
+        .enumerate()
+        .map(|(i, step)| {
+            [
+                Event {
+                    kind: EventKind::Start,
+                    position: *step.z.start(),
+                    step_id: i,
+                },
+                Event {
+                    kind: EventKind::End,
+                    position: *step.z.end(),
+                    step_id: i,
+                },
+            ]
+        })
+        .collect_vec();
 
-    println!("Part 1: {}", result1);
+    let mut active_steps: Vec<usize> = Vec::with_capacity(steps.len());
+    let mut area_start = 0;
+    let mut total_cubes = 0usize;
+    let mut init_cubes = 0usize;
 
-    // TODO: works for my input, is somehow off by 10 for the example input
-    let result2 = {
-        let x_events = steps
+    for event in x_events {
+        let y_events = active_steps
             .iter()
-            .enumerate()
-            .flat_map(|(i, step)| {
-                [
-                    Event {
-                        kind: EventKind::Start,
-                        position: *step.x.start(),
-                        step_id: i,
-                    },
-                    Event {
-                        kind: EventKind::End,
-                        position: *step.x.end(),
-                        step_id: i,
-                    },
-                ]
-            })
+            .flat_map(|&i| y_events[i].clone())
+            // TODO: be smarter and avoid re-sorting every time?
             .sorted_by_key(|event| (event.position, event.kind));
 
-        let y_events = steps
-            .iter()
-            .enumerate()
-            .map(|(i, step)| {
-                [
-                    Event {
-                        kind: EventKind::Start,
-                        position: *step.y.start(),
-                        step_id: i,
-                    },
-                    Event {
-                        kind: EventKind::End,
-                        position: *step.y.end(),
-                        step_id: i,
-                    },
-                ]
-            })
-            .collect_vec();
+        let mut y_cubes = 0;
+        let mut y_init_cubes = 0;
+        {
+            let mut active_steps: Vec<usize> = Vec::with_capacity(active_steps.len());
+            let mut area_start = 0;
 
-        let z_events = steps
-            .iter()
-            .enumerate()
-            .map(|(i, step)| {
-                [
-                    Event {
-                        kind: EventKind::Start,
-                        position: *step.z.start(),
-                        step_id: i,
-                    },
-                    Event {
-                        kind: EventKind::End,
-                        position: *step.z.end(),
-                        step_id: i,
-                    },
-                ]
-            })
-            .collect_vec();
+            for event in y_events {
+                let z_events = active_steps
+                    .iter()
+                    .flat_map(|&i| z_events[i].clone())
+                    // TODO: be smarter and avoid re-sorting every time?
+                    .sorted_by_key(|event| (event.position, event.kind));
 
-        let mut active_steps: Vec<usize> = Vec::with_capacity(steps.len());
-        let mut area_start = 0;
-        let mut total_cubes = 0usize;
+                let mut z_cubes = 0;
+                let mut z_init_cubes = 0;
+                {
+                    let mut active_steps: Vec<usize> = Vec::with_capacity(active_steps.len());
+                    let mut area_start = 0;
 
-        for event in x_events {
-            let y_events = active_steps
-                .iter()
-                .flat_map(|&i| y_events[i].clone())
-                // TODO: be smarter and avoid re-sorting every time?
-                .sorted_by_key(|event| (event.position, event.kind));
-
-            let mut y_cubes = 0;
-            {
-                let mut active_steps: Vec<usize> = Vec::with_capacity(active_steps.len());
-                let mut area_start = 0;
-
-                for event in y_events {
-                    let z_events = active_steps
-                        .iter()
-                        .flat_map(|&i| z_events[i].clone())
-                        // TODO: be smarter and avoid re-sorting every time?
-                        .sorted_by_key(|event| (event.position, event.kind));
-
-                    let mut z_cubes = 0;
-                    {
-                        let mut active_steps: Vec<usize> = Vec::with_capacity(active_steps.len());
-                        let mut area_start = 0;
-
-                        for event in z_events {
-                            if let Some(max) = active_steps.iter().copied().max() {
-                                if steps[max].state {
-                                    z_cubes += match event.kind {
-                                        EventKind::Start => event.position - area_start,
-                                        EventKind::End => event.position - area_start + 1,
-                                    }
+                    for event in z_events {
+                        if let Some(max) = active_steps.iter().copied().max() {
+                            if steps[max].state {
+                                z_cubes += match event.kind {
+                                    EventKind::Start => event.position - area_start,
+                                    EventKind::End => event.position - area_start + 1,
+                                };
+                                z_init_cubes += match event.kind {
+                                    EventKind::Start => std::cmp::max(
+                                        0,
+                                        std::cmp::min(50, event.position)
+                                            - std::cmp::max(-50, area_start),
+                                    ),
+                                    EventKind::End => std::cmp::max(
+                                        0,
+                                        std::cmp::min(50, event.position)
+                                            - std::cmp::max(-50, area_start)
+                                            + 1,
+                                    ),
                                 }
                             }
-
-                            match event.kind {
-                                EventKind::Start => {
-                                    active_steps.push(event.step_id);
-                                    area_start = event.position;
-                                }
-                                EventKind::End => {
-                                    active_steps.swap_remove(
-                                        active_steps
-                                            .iter()
-                                            .position(|&step| step == event.step_id)
-                                            .unwrap(),
-                                    );
-                                    area_start = event.position + 1;
-                                }
-                            };
                         }
+
+                        match event.kind {
+                            EventKind::Start => {
+                                active_steps.push(event.step_id);
+                                area_start = event.position;
+                            }
+                            EventKind::End => {
+                                active_steps.swap_remove(
+                                    active_steps
+                                        .iter()
+                                        .position(|&step| step == event.step_id)
+                                        .unwrap(),
+                                );
+                                area_start = event.position + 1;
+                            }
+                        };
                     }
-
-                    match event.kind {
-                        EventKind::Start => {
-                            active_steps.push(event.step_id);
-                            z_cubes *= event.position - area_start;
-                            area_start = event.position;
-                        }
-                        EventKind::End => {
-                            active_steps.swap_remove(
-                                active_steps
-                                    .iter()
-                                    .position(|&step| step == event.step_id)
-                                    .unwrap(),
-                            );
-                            z_cubes *= event.position - area_start + 1;
-                            area_start = event.position + 1;
-                        }
-                    };
-
-                    y_cubes += z_cubes;
                 }
+
+                match event.kind {
+                    EventKind::Start => {
+                        active_steps.push(event.step_id);
+                        z_cubes *= event.position - area_start;
+                        z_init_cubes *= std::cmp::max(
+                            0,
+                            std::cmp::min(50, event.position) - std::cmp::max(-50, area_start),
+                        );
+                        area_start = event.position;
+                    }
+                    EventKind::End => {
+                        active_steps.swap_remove(
+                            active_steps
+                                .iter()
+                                .position(|&step| step == event.step_id)
+                                .unwrap(),
+                        );
+                        z_cubes *= event.position - area_start + 1;
+                        z_init_cubes *= std::cmp::max(
+                            0,
+                            std::cmp::min(50, event.position) - std::cmp::max(-50, area_start) + 1,
+                        );
+                        area_start = event.position + 1;
+                    }
+                };
+
+                y_cubes += z_cubes;
+                y_init_cubes += z_init_cubes;
             }
-
-            match event.kind {
-                EventKind::Start => {
-                    active_steps.push(event.step_id);
-                    y_cubes *= event.position - area_start;
-                    area_start = event.position;
-                }
-                EventKind::End => {
-                    active_steps.swap_remove(
-                        active_steps
-                            .iter()
-                            .position(|&step| step == event.step_id)
-                            .unwrap(),
-                    );
-                    y_cubes *= event.position - area_start + 1;
-                    area_start = event.position + 1;
-                }
-            };
-
-            total_cubes += y_cubes as usize;
         }
 
-        total_cubes
-    };
+        match event.kind {
+            EventKind::Start => {
+                active_steps.push(event.step_id);
+                y_cubes *= event.position - area_start;
+                y_init_cubes *= std::cmp::max(
+                    0,
+                    std::cmp::min(50, event.position) - std::cmp::max(-50, area_start),
+                );
+                area_start = event.position;
+            }
+            EventKind::End => {
+                active_steps.swap_remove(
+                    active_steps
+                        .iter()
+                        .position(|&step| step == event.step_id)
+                        .unwrap(),
+                );
+                y_cubes *= event.position - area_start + 1;
+                y_init_cubes *= std::cmp::max(
+                    0,
+                    std::cmp::min(50, event.position) - std::cmp::max(-50, area_start) + 1,
+                );
+                area_start = event.position + 1;
+            }
+        };
 
-    println!("Part 2: {}", result2);
+        total_cubes += y_cubes as usize;
+        init_cubes += y_init_cubes as usize;
+    }
+
+    println!("Part 1: {}", init_cubes);
+    println!("Part 2: {}", total_cubes);
 }
